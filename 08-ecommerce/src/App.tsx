dcce16d34e8130bd32418b9d27b11e1e51b3a8c5
Joshua <joxuecont@gmail.com>
2024-04-25 06:15:58 -0400

    create Products component & show list

--- 08-ecommerce/src/App.tsx
+++ 08-ecommerce/src/App.tsx
@@ -1,11 +1,14 @@
-import { products } from './mocks/products.json';
+import { useState } from 'react';
+import { products as initialProducts } from './mocks/products.json';
 import Products from './components/Products';
 
 function App() {
+  const [products] = useState(initialProducts);
+
   return (
     <div className="app">
       <header>
-        <h1>eCommerce Store 🛒</h1>
+        <h1>React Shop 🛒</h1>
       </header>
 
       <Products products={products} />
--- 08-ecommerce/src/components/Products.tsx
+++ 08-ecommerce/src/components/Products.tsx
@@ -1,4 +1,6 @@
 import { Product } from '../types';
+import { AddToCartIcon } from './Icons';
+import './Products.css';
 
 type ProductsProps = {
   products: Product[];
@@ -9,7 +11,18 @@ function Products({ products }: ProductsProps) {
     <main className="products">
       <ul>
         {products.slice(0, 10).map((product) => (
-          <li key={product.id}>{product.title}</li>
+          <li key={product.id}>
+            <img src={product.thumbnail} alt={product.title} />
+            <div>
+              <h3>{product.title}</h3>
+              <span>${product.price}</span>
+            </div>
+            <div>
+              <button>
+                <AddToCartIcon />
+              </button>
+            </div>
+          </li>
         ))}
       </ul>
     </main>
