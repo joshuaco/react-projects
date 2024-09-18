790cf5ce667c3b2a9b12a1fd350d0877ae595162
Joshua <joxuecont@gmail.com>
2024-09-18 19:45:54 -0400

    add type guard to action middleware

--- 14-redux-crud/src/store/middlewares.ts
+++ 14-redux-crud/src/store/middlewares.ts
@@ -1,6 +1,7 @@
 import { Middleware } from '@reduxjs/toolkit';
 import { rollbackUser } from './users/slice';
 import { toast } from 'sonner';
+import { RootState } from '.';
 
 export const persistancleLocalStorageMiddleware: Middleware =
   (store) => (next) => (action) => {
@@ -11,10 +12,17 @@ export const persistancleLocalStorageMiddleware: Middleware =
     localStorage.setItem('__redux__state', JSON.stringify(store.getState()));
   };
 
+const isAction = (
+  action: unknown
+): action is { type: string; payload: string } =>
+  typeof action === 'object' && action !== null && 'type' in action;
+
 export const syncWithDatabaseMiddleware: Middleware =
   (store) => (next) => (action) => {
+    if (!isAction(action)) return next(action);
+
     const { type, payload } = action;
-    const prevState = store.getState();
+    const prevState = store.getState() as RootState;
     next(action);
 
     if (type === 'users/removeUserByID') {
