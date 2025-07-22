cb45570ed065716fe0c2bf638183f2dce682f18e
Joshua <joxuecont@gmail.com>
2025-07-22 15:13:31 -0400

    Refactor application structure by removing App component and introducing Router for navigation. Add MainLayout and ListPage components for better organization. Integrate lucide-react and react-router dependencies for enhanced UI and routing capabilities.

--- 21-github-issues/src/main.tsx
+++ 21-github-issues/src/main.tsx
@@ -2,16 +2,19 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import { BrowserRouter } from 'react-router';
 import './index.css';
-import App from './App.tsx';
+import Router from './router.tsx';
 
 const queryClient = new QueryClient();
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
-      <App />
-      <ReactQueryDevtools initialIsOpen={false} />
+      <BrowserRouter>
+        <Router />
+        <ReactQueryDevtools initialIsOpen={false} />
+      </BrowserRouter>
     </QueryClientProvider>
   </StrictMode>
 );
---
+++ 21-github-issues/src/pages/list-page.tsx
@@ -0,0 +1,11 @@
+import IssueList from '@/components/issue-list';
+
+function ListPage() {
+  return (
+    <section>
+      <IssueList />
+    </section>
+  );
+}
+
+export default ListPage;
