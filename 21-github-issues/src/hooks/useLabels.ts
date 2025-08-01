df4267955da4614b9c2e50c0e0951d7d5647f78b
Joshua <joxuecont@gmail.com>
2025-08-01 11:21:22 -0400

    Add stale time configuration for labels and create IssuePage component

--- 21-github-issues/src/hooks/useLabels.ts
+++ 21-github-issues/src/hooks/useLabels.ts
@@ -5,6 +5,7 @@ export const useLabels = () => {
   const { data: labels } = useQuery({
     queryKey: ['labels'],
     queryFn: getLabels,
+    staleTime: 1000 * 60 * 60, // 1 hour stale time
   });
 
   return { labels };
---
+++ 21-github-issues/src/pages/issue-page.tsx
@@ -0,0 +1,9 @@
+function IssuePage() {
+  return (
+    <div>
+      <p>Issue Page</p>
+    </div>
+  );
+}
+
+export default IssuePage;
